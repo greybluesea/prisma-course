997ee2f5324dec24689d7d03af9b7120e9db8409
Tony <greybluesea@gmail.com>
2023-07-26 13:47:23 +1200

    pagination done

---
+++ app/api/post/pagination/route.ts
@@ -0,0 +1,31 @@
+import prisma from "@/lib/prisma";
+
+// offset based
+/* export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const page = +(searchParams.get("page") ?? 1);
+  const size = +(searchParams.get("size") ?? 3);
+
+  const posts = await prisma.post.findMany({
+    skip: (page - 1) * size,
+    take: size,
+  });
+
+  return new Response(JSON.stringify(posts));
+} */
+
+// cursor based
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const cursor = +(searchParams.get("cursor") ?? 1);
+  const size = +(searchParams.get("size") ?? 3);
+
+  const posts = await prisma.post.findMany({
+    cursor: {
+      id: cursor,
+    },
+    take: size,
+  });
+
+  return new Response(JSON.stringify(posts));
+}
