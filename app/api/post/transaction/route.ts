0badb6d55d4bd1c53987173ebbcd87a4de0b88ef
Tony <greybluesea@gmail.com>
2023-07-27 22:58:21 +1200

    'transaction' done

---
+++ app/api/post/transaction/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/prisma";
+
+export async function PATCH() {
+  const withDrawUpdate = prisma.post.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      likeNum: {
+        decrement: 5,
+      },
+    },
+  });
+
+  const depositUpdate = prisma.post.update({
+    where: {
+      id: 2,
+    },
+    data: {
+      likeNum: {
+        increment: 5,
+      },
+    },
+  });
+
+  const result = await prisma.$transaction([withDrawUpdate, depositUpdate]);
+
+  return new Response(JSON.stringify(result));
+}
